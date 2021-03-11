4d9395063ac41ce509733168f5644d589951148f
Vincentius Maurich <maurichkian@gmail.com>
2021-03-12 00:56:31 +0700

    Soal 1

--- I0320108_soal1_tugas2.py
+++ I0320108_soal1_tugas2.py
@@ -1,1 +1,60 @@
+### Informasi Program
+print("A. Menghitung Luas Persegi Panjang")
 
+# Input Panjang dan Lebar
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+# Penghitungan
+luas_persegipanjang = panjang * lebar
+
+# Hasil Penghitungan
+print("Luas persegi panjang = ", luas_persegipanjang)
+
+### Informasi Program
+print("B. Menghitung Luas Lingkaran")
+
+# Input Jari-jari
+r = float(input("Masukkan jari-jari: "))
+
+# Penghitungan
+luas_lingkaran = 3.14 * (r ** 2)
+
+# Hasil Penghitungan
+print("Luas lingkaran = ", luas_lingkaran)
+
+### Informasi Program
+print("C. Menghitung Luas Kubus")
+
+# Input Panjang Sisi
+s = float(input("Masukkan panjang sisi: "))
+
+# Penghitungan
+luas_kubus = 6 * (s ** 2)
+
+# Hasil Penghitungan
+print("Luas kubus = ", luas_kubus)
+
+### Informasi Program
+print("D. Menghitung Konversi Suhu Celcius ke Fahrenheit")
+
+# Input Suhu Celcius
+C = float(input("Masukkan suhu celcius: "))
+
+# Penghitungan
+F = (C * (9/5)) + 32
+
+# Hasil Penghitungan
+print("Suhu dari", C, "celcius = ", F "fahrenheit")
+
+### Informasi Program
+print("E. Menghitung Konversi Suhu Reamur ke Kelvin")
+
+# Input Suhu Reamur
+R = float(input("Masukkan suhu reamur: "))
+
+# Penghitungan
+K = (R * (5/4)) + 273.15
+
+# Hasil Penghitungan
+print("Suhu dari", R, "reamur = ", K, "kelvin")
